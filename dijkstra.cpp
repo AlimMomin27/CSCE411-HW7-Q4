8b0e8db95dc98681bb44c6f0e7fe9e800f7f9a94
alimmomin270@gmail.com <53840493+AlimMomin27@users.noreply.github.com>
2025-03-21 12:24:44 -0500

    Completed, fixed heap issue with downheap, made adj list for Eppes scenario. Formated output in terminal.

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -55,9 +55,11 @@ class MPQ {
         int r_child = right_child(index);
 
         //handles vector of size > 2
+
         if(r_child < C.size())
         {
-            if(l_child < C.size() &&  comp(C.at(index),C.at(l_child)) && comp(C.at(r_child), C.at(l_child)))
+            //very long third check is for when the heap is filled with equal values, the downheap will still occur even when both children are the samevalue.
+            if(l_child < C.size() &&  comp(C.at(index),C.at(l_child)) && (( comp(C.at(r_child), C.at(l_child))) || (!comp(C.at(l_child), C.at(r_child)) && !comp(C.at(r_child), C.at(l_child)))))
             {
                 std::swap(C.at(index), C.at(l_child));
                 downheap(l_child);
@@ -115,16 +117,18 @@ class MPQ {
             if(C.at(i).first == queue_item.first)
             {
                 C[i].second = weight;
-                //checking the children nodes because updating key will corrupt the MPQ structure
-                if(left_child(i) < C.size() && ((C.at(left_child(i)).second < weight) || (right_child(i) < C.size() && C.at(right_child(i)).second < weight  )))
-                {
-                    downheap(i);
-                }
+
                 //checks parents as well, if the key is less than the parent then upheap to correct position.
-                else if(weight < C.at(parent(i)).second)
+                if(weight < C.at(parent(i)).second)
                 {
                     upheap(i);
                 }
+                //checking the children nodes because updating key will corrupt the MPQ structure
+                else if(left_child(i) < C.size() && ((C.at(left_child(i)).second < weight) || (right_child(i) < C.size() && C.at(right_child(i)).second < weight  )))
+                {
+                    downheap(i);
+                }
+                
                 return true;
             }
         }
@@ -158,7 +162,7 @@ class MPQ {
 };
 
 //Dijkstra Class
-template<typename T>
+template<typename T, class Compare = std::less<T>>
 class Dijkstra {
     //using 3 of the same data_type to have meaningful difference when specifying datatypes for structures.
     using edge = std::pair<T, int>;
@@ -167,7 +171,9 @@ class Dijkstra {
     // using a unordered_map for adj_list, contains a pair that has the node being pointed to and its edge weight
     using adjacency_list = std::vector<edge>;
 
-    MPQ<queue_item, std::vector<queue_item>, custom_comp<T, int>> Q;
+    Compare comp;
+
+    MPQ<queue_item, std::vector<queue_item>, custom_comp<T,int>> Q;
     std::unordered_map<T, parent_and_weight> S; // tracks a node's parent includes the nodes total weight
 
     public:
@@ -184,27 +190,24 @@ class Dijkstra {
             }
             else
             {
-                Q.push(std::pair<T,int>(G[i], INT_MAX));
+                Q.push(std::pair<T,int>(G[i], 100000)); //I chose 100,000 instead of any built in inf or INTMAX (overflow issues)
             }
         }
 
         while( !Q.empty() )
         {
             queue_item u = Q.top();
-            std::cout << " REMOVED " << u.first <<  "FROM Q\n";
             Q.pop(); //remove item from top of the queue
             
 
             for( edge v : adj_list[u.first]){
                 //Call RELAX --> update Key for my implementation
                 bool updated = Q.updateKey(v, (u.second + v.second));
-                std::cout << "UPDATE: " << updated << " FOR " << v.first << std::endl;
+                //std::cout << " UPDATE: " << updated << " FOR " << v.first << " --> " << u.second+v.second << std::endl;
                 if(updated){
                     S[v.first] = parent_and_weight(u.first, {u.second + v.second});
                 }
             }
-            Q.print_PQ();
-
         }
         return S;
     }
@@ -222,7 +225,7 @@ int main(){
     q.updateKey({"B", 0}, 2);
     q.print_PQ();
     */
-    //TESTING DIJKSTRA
+    /* TESTING DIJKSTRA (using strings and interger weights)
     Dijkstra<std::string> Djk;
     std::vector<std::string> streets = {"University Drive", "Ross St", "Houston St", "George Bush Drive", "Wellborn St"};
     std::unordered_map<std::string, std::vector<std::pair<std::string, int>>> adj;
@@ -238,6 +241,42 @@ int main(){
     {
         std::cout << "(" << pr.first << ", P[" << pr.second.first << "], " << pr.second.second << ")\n";
     }
+    */
+    //Dijkstra for Professor Charlie Eppes. o7
+    Dijkstra<int> Djk;
+    std::vector<int> streets = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22};
+    std::unordered_map<int, std::vector<std::pair<int,int>>> adj;
+    adj[1] = {{2,1}, {11,1}};
+    adj[2] = {{1,1},{3,1},{21,1}};
+    adj[3] = {{2,1},{4,1},{8,2}};
+    adj[4] = {{3,1},{5,1}};
+    adj[5] = {{4,1},{6,2},{7,1},{22,1}};
+    adj[6] = {{5,2},{7,1}};
+    adj[7] = {{5,1},{6,1},{8,1}};
+    adj[8] = {{3,2},{7,1},{9,1}};
+    adj[9] = {{8,1},{10,1},{19,2}};
+    adj[10] = {{9,1},{11,1},{18,2}};
+    adj[11] = {{1,1},{10,1},{12,2},{17,1}};
+    adj[12] = {{11,2},{13,2}};
+    adj[13] = {{12,2},{14,2},{21,1}};
+    adj[14] = {{13,2},{15,1},{16,1},{20,1}};
+    adj[15] = {{14,1}};
+    adj[16] = {{14,1},{17,2}};
+    adj[17] = {{11,1},{16,2},{18,2}};
+    adj[18] = {{10,2},{17,2},{19,2}};
+    adj[19] = {{9,2},{18,2}};
+    adj[20] = {{14,1},{21,2},{22,2}};
+    adj[21] = {{2,1},{13,1},{20,2},{22,2}};
+    adj[22] = {{5,1},{20,2},{21,2}};
+
+    std::unordered_map<int, std::pair<int, int>> returned_set = Djk.dijkstra(streets, adj, 22);
+    std::cout << " ---- (NODE, [PARENT_NODE], TOTAL WEIGHT) ----\n";
+    std::cout << " ----     Source node is NOT printed      ----\n";
+    for(std::pair<int, std::pair<int, int>> pr : returned_set)
+    {
+        std::cout << "(" << pr.first << ", P[" << pr.second.first << "], " << pr.second.second << ")\n";
+    }
+
 
     return 0;
 }
