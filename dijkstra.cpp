2de78a84c31a63f70486abf5204806dbb1ae294e
alimmomin270@gmail.com <53840493+AlimMomin27@users.noreply.github.com>
2025-03-21 00:06:54 -0500

    Added downheap, pop, top, empty.

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -11,6 +11,7 @@
 
 //Using templating to allow for dynamic usages for datatypes,
 //I.E. std::pair<string, int> where its "Bizzel St." with a weight of 20.
+//MPQ template inspired from CSCE221 PA6 (we built a MPQ using these functions so the functions are used from there)
 template<typename T = std::pair<int,int>, class Container = std::vector<T>, class Compare = std::less<T>>
 class MPQ {
     private:
@@ -21,6 +22,8 @@ class MPQ {
     int left_child(int index) { return 2 * index + 1; }
     int right_child(int index) { return 2 * ( index + 1 ); }
     int parent(int index) { return (index - 1) / 2; }
+    //extra function for nodes to check if they're leaf nodes 
+    bool is_leaf(int index) {return left_child(index) >= C.size(); }
 
 
     //upheap -- internal function
@@ -33,7 +36,36 @@ class MPQ {
         }
     }
 
-    //downheap
+    //downheap -- internal function
+    void downheap(int index){
+        if(is_leaf(index)){ return; }
+        int l_child = left_child(index);
+        int r_child = right_child(index);
+
+        //handles vector of size > 2
+        if(r_child < C.size())
+        {
+            if(l_child < C.size() &&  comp(C.at(index),C.at(l_child)) && comp(C.at(r_child), C.at(l_child)))
+            {
+                std::swap(C.at(index), C.at(l_child));
+                downheap(l_child);
+            }
+            else if(r_child < C.size() && comp(C.at(index),C.at(r_child)) && comp(C.at(l_child), C.at(r_child)))
+            {
+                std::swap(C.at(index), C.at(r_child));
+                downheap(r_child);
+            }
+        }
+        else //vector of size 2 or less
+        {
+            if(l_child < C.size() && comp(C.at(index),C.at(l_child)))
+            {
+                std::swap(C.at(index), C.at(l_child));
+                downheap(l_child);
+            }
+        }
+       
+    }
 
     public:
         MPQ() = default;
@@ -50,17 +82,39 @@ class MPQ {
         upheap(C.size()-1);
     }
 
-    //POP
+    //POP -- swapping the 0 index and last index values, pop_back to remove min/max value, downheap to restore priority queue
+    void pop()
+    {
+        if(empty())
+        {
+            return;
+        }
+        std::swap(C[0], C[C.size()-1]);
+        C.pop_back();
+        downheap(0);
+    }
 
     //UPDATE-KEY : made specifically for dijkstra, reason for custom MPQ and not using stl priority queue
 
-    //TOP
-
-    //EMPTY
+    //TOP -- return the min/max value
+    T top()
+    {
+        if(empty())
+        {
+            return T();
+        }
+        return C[0];
+    }
+    //EMPTY -- retruns boolean depending on size of the container
+    bool empty()
+    {
+        return C.size() == 0;
+    }
 
     //PRINT
     void print_PQ(){
-        for(int i = 0; i < C.size(); i++){
+        for(int i = 0; i < C.size(); i++)
+        {
             std::cout << " (" << C.at(i) << ") \n";
         }
     }
@@ -74,6 +128,14 @@ int main(){
     q.push(1);
     q.push(50);
     q.print_PQ();
+    std::cout << q.top() << std::endl;
+    q.pop();
+    std::cout << q.top() << std::endl;
+    q.pop();
+    std::cout << q.top() << std::endl;
+    q.pop();
+    std::cout << q.top() << std::endl;
+    q.pop();
 
     return 0;
 }
