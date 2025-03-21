5d12470cb8e624402d4ab2dc6e3528cade24fd0d
alimmomin270@gmail.com <53840493+AlimMomin27@users.noreply.github.com>
2025-03-20 23:16:40 -0500

    Custom MPQ with blueprint for functions.

---
+++ dijksta.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <unordered_map>
+#include <limits>
+
+
+template<typename T = std::pair<int,int>, class Container = std::vector<T>, class Compare = std::less<T>>
+class MPQ {
+    private:
+    Container C;
+    Compare comp;
+
+    //upheap
+
+    //downheap
+
+    public:
+        MPQ() = default;
+        MPQ( const MPQ& other ) = default;
+        MPQ( MPQ&& other ) = default;
+        ~MPQ() = default;
+        MPQ& operator=( const MPQ& other ) = default;
+        MPQ& operator=( MPQ&& other ) = default;
+
+    //PUSH
+
+    //POP
+
+    //UPDATE-KEY : made specifically for dijkstra, reason for custom MPQ and not using stl priority queue
+
+    //TOP
+
+    //EMPTY
+
+};
